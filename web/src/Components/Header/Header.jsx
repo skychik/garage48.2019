872c70cd790765705ddf75d116f2e5a183bfead2
Kirill Esakov <kir-esakov2011@mail.ru>
2019-11-17 16:10:23 +0200

    logo in header added

--- web/src/Components/Header/Header.jsx
+++ web/src/Components/Header/Header.jsx
@@ -5,6 +5,10 @@ import './Header.css';
 const Header = (props) => {
 	return (
 		<div className="header">
+			<img src="/img/logo.png" alt="" style={{
+				width: "40px",
+				height: "40px",
+			}}/>
 			<div className="header_content">
 				Tensyteam
 			</div>
